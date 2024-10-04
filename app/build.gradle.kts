1aa1107a630f9a78c4f1354353e1a0aaa4c5449a
Azutillo <vincentazut@gmail.com>
2024-10-04 12:20:37 +0800

    oct1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation ("androidx.cardview:cardview:1.0.0")
+
 }
--- app/src/main/java/com/example/techtally/UserDashboardActivity.kt
+++ app/src/main/java/com/example/techtally/UserDashboardActivity.kt
@@ -28,6 +28,8 @@ class UserDashboardActivity : AppCompatActivity() {
         binding = ActivityUserDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -85,4 +87,5 @@ class UserDashboardActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
 }
