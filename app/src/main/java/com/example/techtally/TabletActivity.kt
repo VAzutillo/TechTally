e0450f1b60ea2f4898ff3fc2f5146d758de36b31
Azutillo <vincentazut@gmail.com>
2024-10-25 17:57:07 +0800

    bago

--- app/src/main/java/com/example/techtally/TabletActivity.kt
+++ app/src/main/java/com/example/techtally/TabletActivity.kt
@@ -26,32 +26,32 @@ class TabletActivity : AppCompatActivity() {
             startActivity(intent)
         }
         // Navigate from TabletActivity to GalaxyTabS10UltraFullDetails
-        val GalaxyTabS10UltraSeeMoreBTN = findViewById<TextView>(R.id.tabletGalaxyTabS10UltraSeeMoreButton)
-        GalaxyTabS10UltraSeeMoreBTN.setOnClickListener {
+        val galaxyTabS10UltraSeeMoreBTN = findViewById<TextView>(R.id.tabletGalaxyTabS10UltraSeeMoreButton)
+        galaxyTabS10UltraSeeMoreBTN.setOnClickListener {
             val intent = Intent(this, GalaxyTabS10UltraFullDetails::class.java)
             startActivity(intent)
         }
         // Navigate from TabletActivity to Ipad13ProFullDetails
-        val Ipad13ProSeeMoreBTN = findViewById<TextView>(R.id.tabletIpad13ProSeeMoreButton)
-        Ipad13ProSeeMoreBTN.setOnClickListener {
+        val ipad13ProSeeMoreBTN = findViewById<TextView>(R.id.tabletIpad13ProSeeMoreButton)
+        ipad13ProSeeMoreBTN.setOnClickListener {
             val intent = Intent(this, Ipad13ProFullDetails::class.java)
             startActivity(intent)
         }
         // Navigate from TabletActivity to OppoPad2FullDetails
-        val OppoPad2SeeMoreBTN = findViewById<TextView>(R.id.tabletOppoPad2SeeMoreButton)
-        OppoPad2SeeMoreBTN.setOnClickListener {
+        val oppoPad2SeeMoreBTN = findViewById<TextView>(R.id.tabletOppoPad2SeeMoreButton)
+        oppoPad2SeeMoreBTN.setOnClickListener {
             val intent = Intent(this, OppoPad2FullDetails::class.java)
             startActivity(intent)
         }
         // Navigate from TabletActivity to RealmePad2FullDetails
-        val RealmePad2SeeMoreBTN = findViewById<TextView>(R.id.tabletRealmePad2SeeMoreButton)
-        RealmePad2SeeMoreBTN.setOnClickListener {
+        val realmePad2SeeMoreBTN = findViewById<TextView>(R.id.tabletRealmePad2SeeMoreButton)
+        realmePad2SeeMoreBTN.setOnClickListener {
             val intent = Intent(this, RealmePad2FullDetails::class.java)
             startActivity(intent)
         }
         // Navigate from TabletActivity to XiaomiPad6ProFullDetails
-        val XiaomiPad6ProSeeMoreBTN = findViewById<TextView>(R.id.tabletXiaomiPad6ProSeeMoreButton)
-        XiaomiPad6ProSeeMoreBTN.setOnClickListener {
+        val xiaomiPad6ProSeeMoreBTN = findViewById<TextView>(R.id.tabletXiaomiPad6ProSeeMoreButton)
+        xiaomiPad6ProSeeMoreBTN.setOnClickListener {
             val intent = Intent(this, XiaomiPad6ProFullDetails::class.java)
             startActivity(intent)
         }
